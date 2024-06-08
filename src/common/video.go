894b419cc9fb10e9c0cf02b04dd1b5928d290cef
Dragoslav Tamindžija <dragodev@protonmail.com>
2024-06-08 19:00:19 +0200

    feat: add json annotation to common

--- src/common/video.go
+++ src/common/video.go
@@ -7,59 +7,61 @@ import (
 
 type Video struct {
     // path on S3
-    FileName string             `dynamodbav:"fileName"`
-    FileType string             `dynamodbav:"fileType"`
+    FileName           string `dynamodbav:"fileName" json:"fileName"`
+    FileType           string `dynamodbav:"fileType" json:"fileType"`
     // size in bytes
-    FileSize uint64             `dynamodbav:"fileSize"`
-    CreationTimestamp int64     `dynamodbav:"creationTimestamp"`
-    LastChangeTimestamp int64   `dynamodbav:"lastChangeTimestamp"`
+    FileSize           uint64 `dynamodbav:"fileSize" json:"fileSize"`
+    CreationTimestamp  int64  `dynamodbav:"creationTimestamp" json:"creationTimestamp"`
+    LastChangeTimestamp int64 `dynamodbav:"lastChangeTimestamp" json:"lastChangeTimestamp"`
 }
 
 type Episode struct {
-    EpisodeNumber uint64        `dynamodbav:"episodeNumber"`
-    Title string                `dynamodbav:"title"`
-    Description string          `dynamodbav:"description"`
+    EpisodeNumber uint64  `dynamodbav:"episodeNumber" json:"episodeNumber"`
+    Title         string  `dynamodbav:"title" json:"title"`
+    Description   string  `dynamodbav:"description" json:"description"`
     // String Set type
-    Actors []string             `dynamodbav:"actors"`
+    Actors        []string `dynamodbav:"actors" json:"actors"`
     // String Set type
-    Directors []string          `dynamodbav:"directors"`
-    Video Video                 `dynamodbav:"video"`
+    Directors     []string `dynamodbav:"directors" json:"directors"`
+    Video         Video    `dynamodbav:"video" json:"video"`
 }
 
 type Season struct {
-    SeasonNumber uint64        `dynamodbav:"number"`
+    SeasonNumber uint64    `dynamodbav:"number" json:"seasonNumber"`
     // List Type
-    Episodes []Episode         `dynamodbav:"episodes"`
+    Episodes     []Episode `dynamodbav:"episodes" json:"episodes"`
 }
-
 
 //NOTE: UUID is primary key, generated using google/uuid
 type Movie struct {
     // google/uuid
-    UUID string                 `dynamodbav:"id"`
-    Title string                `dynamodbav:"title"`
-    Description string          `dynamodbav:"description"`
+    UUID         string    `dynamodbav:"id" json:"id"`
+    Title        string    `dynamodbav:"title" json:"title"`
+    Description  string    `dynamodbav:"description" json:"description"`
     // String Set type
-    Genres []string             `dynamodbav:"genres"`
+    Genres       []string  `dynamodbav:"genres" json:"genres"`
     // String Set type
-    Actors []string             `dynamodbav:"actors"`
+    Actors       []string  `dynamodbav:"actors" json:"actors"`
     // String Set type
-    Directors []string          `dynamodbav:"directors"`
-    Video Video                 `dynamodbav:"video"`
+    Directors    []string  `dynamodbav:"directors" json:"directors"`
+    Video        Video     `dynamodbav:"video" json:"video"`
 }
 
 //NOTE: UUID is primary key, generated using google/uuid
 type Show struct {
     // google/uuid
-    UUID string                 `dynamodbav:"id"`
-    Title string                `dynamodbav:"title"`
-    Description string          `dynamodbav:"description"`
+    UUID         string    `dynamodbav:"id" json:"id"`
+    Title        string    `dynamodbav:"title" json:"title"`
+    Description  string    `dynamodbav:"description" json:"description"`
     // String Set type
-    Genres []string             `dynamodbav:"genres"`
+    Genres       []string  `dynamodbav:"genres" json:"genres"`
     // String Set type
-    Actors []string             `dynamodbav:"actors"`
+    Actors       []string  `dynamodbav:"actors" json:"actors"`
     // String Set type
-    Directors []string          `dynamodbav:"directors"`
+    Directors    []string  `dynamodbav:"directors" json:"directors"`
     // List Type
-    Seasons []Season            `dynamodbav:"seasons"`
+    Seasons      []Season  `dynamodbav:"seasons" json:"seasons"`
 }
+
+const VideoBucketName = "argon-videos-bucket"
+const MovieTableName = "movie"
