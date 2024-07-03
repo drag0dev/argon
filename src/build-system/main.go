621a97bf913b6da79ec44e05b58745fe8e4684c2
Dragoslav Tamindžija <dragodev@protonmail.com>
2024-07-03 21:21:54 +0200

    feat: find all lambdas

--- src/build-system/main.go
+++ src/build-system/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "os"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 func help() {
     println("Usage: go run main.go <command>")
@@ -8,6 +12,31 @@ func help() {
     println("build - build and zip all lambdas")
     println("ffmpeg - build and zip ffmpeg transocder")
     println("<lambda-name> - build and zip a specific lambda")
+}
+
+func getAllLambdas() []string {
+    dir, err := os.Open("../")
+    if (err != nil) {
+        fmt.Printf("Cannot open src directory: %v\n", err)
+        os.Exit(-1)
+    }
+    defer dir.Close()
+
+    entries, err := dir.Readdir(0)
+    if (err != nil) {
+        fmt.Printf("Error reading src directory contents: %v\n", err)
+        os.Exit(-1)
+    }
+
+    var lambdas []string
+
+    for _, entry := range entries {
+        fileName := entry.Name()
+        if (strings.HasPrefix(fileName, "lambda-")) {
+            lambdas = append(lambdas, fileName)
+        }
+    }
+    return lambdas
 }
 
 func main() {
@@ -20,6 +49,7 @@ func main() {
     if (command == "help") {
         help()
     } else if (command == "build") {
+        lambdas := getAllLambdas()
     } else if (command == "ffmpeg") {
     } else {
         // build a specific lambda
