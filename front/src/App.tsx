9abefc3edd5f602a1f502a1519cbf5d0af4ffb77
tedord25 <djuric.teodor25@gmail.com>
2024-07-04 15:08:45 +0200

    move aboutpage

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,17 +1,9 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
 
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-
-const About = () => {
-  return (
-    <div className="content">
-      <h1>About Rsbuild</h1>
-      <p>Learn more about Rsbuild and how it can help you build better apps.</p>
-    </div>
-  );
-};
 
 const App = () => {
   return (
@@ -22,7 +14,7 @@ const App = () => {
         </nav>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<About />} />
+          <Route path="/about" element={<AboutPage />} />
         </Routes>
       </div>
     </Router>
