51d07d2b72978f054695f36ec0a53abcfff61769
tedord25 <djuric.teodor25@gmail.com>
2024-07-05 11:30:11 +0200

    rename series to tv show

--- front/src/components/NavBar.tsx
+++ front/src/components/NavBar.tsx
@@ -37,22 +37,22 @@ const NavBar = () => {
               <Link to="/movie/add" className="navbar-item">
                 Add movie
               </Link>
-              <Link to="/series/add" className="navbar-item">
-                Add series
+              <Link to="/tvshow/add" className="navbar-item">
+                Add tvshow
               </Link>
               <hr className="navbar-divider" />
               <Link to="/movie/edit" className="navbar-item">
                 Edit movie
               </Link>
-              <Link to="/series/edit" className="navbar-item">
+              <Link to="/tvshow/edit" className="navbar-item">
                 Edit show
               </Link>
               <hr className="navbar-divider" />
               <Link to="/movie/delete" className="navbar-item">
                 Delete movie
               </Link>
-              <Link to="/series/delete" className="navbar-item">
-                Delete series/season/episode
+              <Link to="/tvshow/delete" className="navbar-item">
+                Delete tvshow/season/episode
               </Link>
             </div>
           </div>
