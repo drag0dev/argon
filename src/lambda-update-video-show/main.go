60f0ebda1bb02f031324b20f6a7b1967d0531e91
Dragoslav Tamindžija <dragodev@protonmail.com>
2024-07-06 20:22:30 +0200

    fix: all lambdas final return

--- src/lambda-update-video-show/main.go
+++ src/lambda-update-video-show/main.go
@@ -182,7 +182,13 @@ func updateShow(ctx context.Context, incomingRequest events.APIGatewayProxyReque
 
     return events.APIGatewayProxyResponse{
         StatusCode: http.StatusOK,
-        Headers:    map[string]string{"Content-Type": "application/json"},
+        Headers: map[string]string{
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": "true",
+            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+            "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+        },
         Body: string(resString),
     }, nil
 }
