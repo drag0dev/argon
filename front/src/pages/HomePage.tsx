202855a5a38cb2291a86834ace5b968a4c1ee834
tedord25 <djuric.teodor25@gmail.com>
2024-07-06 07:35:12 +0200

    minor styling

--- front/src/pages/HomePage.tsx
+++ front/src/pages/HomePage.tsx
@@ -3,17 +3,25 @@ import HomeFeed from '../components/HomeFeed';
 
 const HomePage = () => {
   return (
-    <div style={{ minHeight: '100vh' }}>
-      <section className="hero is-primary is-fullheight">
-        <div className="hero-body">
-          <div className="container">
-            <h1 className="title is-1">Welcome to the Movie/TV Show Tracker</h1>
-            <h2 className="subtitle is-3">Keep track of your favorite movies and TV shows</h2>
-            <HomeFeed />
-          </div>
+    <section className="section">
+      <div className="container">
+        <div style={{ minHeight: '100vh' }}>
+          <section className="hero is-secondary">
+            <div className="hero-body">
+              <div className="container">
+                <h1 className="title is-1">
+                  Welcome to the Movie/TV Show Tracker
+                </h1>
+                <h2 className="subtitle is-3">
+                  Keep track of your favorite movies and TV shows
+                </h2>
+              </div>
+            </div>
+          </section>
+          <HomeFeed />
         </div>
-      </section>
-    </div>
+      </div>
+    </section>
   );
 };
 
