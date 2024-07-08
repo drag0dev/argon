2d96372363dabb84ba6cf0e02a2faa7eb3e22ce2
Dragoslav Tamindžija <dragodev@protonmail.com>
2024-07-08 16:23:41 +0200

    fix: dead code

--- front/src/aws-export.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const awsconfig = {
-  //aws_project_region: 'eu-central-1',
-  //aws_user_pools_id: process.env.POOL_ID,
-  //aws_user_pools_web_client_id: process.env.POOL_CLIENT_ID,
-};
-
-export default awsconfig;
